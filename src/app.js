9fd75995f84a9faab72d524cec83550578e9aa02
prisalvarado18 <palvarado9700@uta.edu.ec>
2023-07-01 12:19:53 -0500

    Added explanations

--- src/app.js
+++ src/app.js
@@ -22,19 +22,35 @@ app.use(bodyParser.json());
         message: 'It works!'
     });
 });*/
+/**Defines a middleware that will be applied to all incoming requests in the Express application. 
+ * This middleware takes three parameters: 
+ * 1. req (request object)
+ * 2. res (response object), 
+ * 3. next (function to pass control to the next middleware). */
 app.use((req, res, next) => {
+    /**This allows any origin (domain) to have access to the application's resources through 
+     * Cross-Origin Resource Sharing (CORS). By setting it to '*', access is allowed from any origin. */
 	res.header('Access-Control-Allow-Origin', '*');
+    /**This line sets the response header 'Access-Control-Allow-Headers' with a list of allowed headers in CORS requests. 
+     * These headers include 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', and 'Authorization'. 
+     * By defining these headers, it allows browsers to send requests with these headers to the application. */
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	);
 	if (req.method === 'OPTIONS') {
+        /**This condition checks if the request method is 'OPTIONS'. 
+         * The 'OPTIONS' request is used to verify the allowed communication options between the client and the server. 
+         * If the request is 'OPTIONS', it sets the 'Access-Control-Allow-Methods' header with a list of allowed methods, including 'PUT', 'POST', 'PATCH', 'DELETE', and 'GET'. Then, it returns a response with a status code of 200 and an empty JSON object {}. 
+         * This indicates that the mentioned methods are allowed in subsequent requests. */
 		res.header(
 			'Access-Control-Allow-Methods',
 			'PUT, POST, PATCH, DELETE, GET'
 		);
 		return res.status(200).json({});
 	}
+    /**This line calls the next() function, allowing the request to proceed to the next middleware in the request handling chain. 
+     * This is important to ensure that requests are properly processed and continue to other middleware or routes defined in the application. */
     next();
 });
 
